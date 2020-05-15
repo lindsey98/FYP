ea22b3168ede5ee885e5a67406540c5c03cf0039
WANG CHAO <1229983126@qq.com>
2020-05-15 23:17:36 +0800

    Add Siamese Result

--- siamese/Siamese_debug.py
+++ siamese/Siamese_debug.py
@@ -76,59 +76,77 @@ def load_model():
     return model
 
 
+def visualize_gradient_against_cosine():
+    model.load_state_dict(state_dict)
+    model.to(device)
+    '''feed screenshot and most similar logo into network and get the embeddings'''
+    img_o_feat = pred(img_o_path, model, imshow=False, title='Sampled Yolo box')
+    img_s_feat = pred(img_s_path, model, imshow=False, title="Most similar logo")
+
+    print(img_o_feat.dot(img_s_feat))
+
+    img_o = procees_image(img_o_path, imshow=False, title=None)
+    img_s = procees_image(img_s_path, imshow=False, title=None)
+
+    # From src to dest
+    model.train()
+    img = Variable(img_o, requires_grad=True)
+
+    img_feat = model(img).squeeze(0)
+
+    loss = img_feat.dot(img_s_feat)
+    loss.backward()
+    grad = img.grad.abs()
+
+    max = grad.view(-1).max()
+
+    grad /= max
+
+    plt.matshow(grad.squeeze(0).squeeze(0).cpu())
+    plt.title('cosine similarity for source')
+
+    model.load_state_dict(state_dict)
+    img = Variable(img_s, requires_grad=True)
+
+    img_feat = model(img).squeeze(0)
+
+    loss = img_feat.dot(img_o_feat)
+    loss.backward()
+    grad = img.grad.abs()
+
+    max = grad.view(-1).max()
+
+    grad /= max
+
+    plt.matshow(grad.squeeze(0).squeeze(0).cpu())
+    plt.title('cosine similarity for dest')
+    plt.show()
+
+
 if __name__ == "__main__":
     '''Configuration'''
     batch_k = 5
     emb_dim = 128
     os.sep = '/'
     model_name = './dws_checkpoint_gray_v6.pth.tar'
-    pair_path = './similar_pair/0/' # store the protected logos for 182 brands
+    pair_path = './similar_pair_p_a_d/4/' # store the protected logos for 182 brands
 
     '''Initialize model and load state dictionary'''
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = load_model()
+
     if device == 'cpu':
-        model.load_state_dict(torch.load(model_name, map_location='cpu'))
+        state_dict = torch.load(model_name, map_location='cpu')
     else:
-        model.load_state_dict(torch.load(model_name))
-    model.to(device)
+        state_dict = torch.load(model_name)
+
+    img_o_path = pair_path + 'logo.png'
+    img_s_path = pair_path + 'yolo_box.png'
+
+    visualize_gradient_against_cosine()
     # model.eval()
 
-    '''feed screenshot and most similar logo into network and get the embeddings'''
-    img_feat = pred(pair_path + 'cropped.png', model, imshow=False, title='Sampled Yolo box')
-    most_sim_logo_feat = pred(pair_path + 'pred_target.png', model, imshow=False, title="Most similar logo")
-    sim = most_sim_logo_feat.dot(img_feat)
 
-    difference = img_feat - most_sim_logo_feat
-    lst = list()
-    for i in range(128):
-        lst.append((i, abs(difference[i])))
-
-
-    img_o = procees_image(pair_path + 'cropped.png', imshow=False, title=None)
-    img_s = procees_image(pair_path + 'pred_target.png', imshow=False, title=None)
-
-    model.train()
-    img = Variable(img_s, requires_grad=True)
-    logo_feat = model(img).squeeze(0)
-
-    t = logo_feat.dot(img_feat)
-
-    t.backward()
-
-    grad = img.grad
-
-    max = grad.view(-1).max()
-    min = grad.view(-1).min()
-
-    grad -= min
-    grad /= (max - min)
-
-
-
-    plt.matshow(grad.squeeze(0).squeeze(0).cpu())
-    plt.title('similarity')
-    plt.show()
 
 # import cv2
 # cv2.imwrite('./test1.png', grad)
