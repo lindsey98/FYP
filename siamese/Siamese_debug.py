e9f9a8f295747d1d756aa14f65368268726691f3
WANG CHAO <1229983126@qq.com>
2020-05-15 21:14:21 +0800

    Allow debugging

--- siamese/Siamese_debug.py
+++ siamese/Siamese_debug.py
@@ -1,10 +1,11 @@
 import os
 import numpy as np
 from PIL import Image, ImageOps
+from torch.autograd import Variable
 from torchvision import transforms
 import matplotlib.pyplot as plt
 import torchvision.models as models
-from torch import nn
+from torch import nn, autograd
 import torch.nn.functional as F
 import torch
 
@@ -29,16 +30,18 @@ class MarginNet(nn.Module):
         x = self.conv(x)
         x = self.base_net(x)  # renet18 embedding
         x = self.dense1(x)
+
+        x = self.normalize(x)
         return x
 
 
-def pred(img_path, model, imshow=False, title=None):
+def procees_image(img_path, imshow=False, title=None):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     with torch.no_grad():
         img = Image.open(os.path.join(img_path)).convert("L")
         img = ImageOps.expand(img, (
-        (max(img.size) - img.size[0]) // 2, (max(img.size) - img.size[1]) // 2, (max(img.size) - img.size[0]) // 2,
-        (max(img.size) - img.size[1]) // 2), fill=255)
+            (max(img.size) - img.size[0]) // 2, (max(img.size) - img.size[1]) // 2, (max(img.size) - img.size[0]) // 2,
+            (max(img.size) - img.size[1]) // 2), fill=255)
         img = img.resize((100, 100))
         img = np.asarray(img)
         if imshow:
@@ -48,9 +51,15 @@ def pred(img_path, model, imshow=False, title=None):
         to_tensor = transforms.ToTensor()
         img = to_tensor(img)
         img = img.unsqueeze(0)
-        img = img.to(device)
-        logo_feat = model(img)
-        logo_feat = l2_norm(logo_feat).squeeze(0).cpu().numpy()
+        return img.to(device)
+
+
+def pred(img_path, model, imshow=False, title=None):
+    img = procees_image(img_path, imshow=False, title=None)
+    img = Variable(img)
+    logo_feat = model(img)
+
+    logo_feat = logo_feat.squeeze(0)
     return logo_feat
 
 
@@ -68,7 +77,6 @@ def load_model():
 
 
 if __name__ == "__main__":
-
     '''Configuration'''
     batch_k = 5
     emb_dim = 128
@@ -84,13 +92,64 @@ if __name__ == "__main__":
     else:
         model.load_state_dict(torch.load(model_name))
     model.to(device)
-    model.eval()
+    # model.eval()
 
     '''feed screenshot and most similar logo into network and get the embeddings'''
-    img_feat = pred(pair_path + 'cropped.png', model, imshow=True, title='Sampled Yolo box')
-    most_sim_logo_feat = pred(pair_path + 'pred_target.png', model, imshow=True, title="Most similar logo")
+    img_feat = pred(pair_path + 'cropped.png', model, imshow=False, title='Sampled Yolo box')
+    most_sim_logo_feat = pred(pair_path + 'pred_target.png', model, imshow=False, title="Most similar logo")
     sim = most_sim_logo_feat.dot(img_feat)
-    print(sim)
+
+    difference = img_feat - most_sim_logo_feat
+    lst = list()
+    for i in range(128):
+        lst.append((i, abs(difference[i])))
+
+
+    img_o = procees_image(pair_path + 'cropped.png', imshow=False, title=None)
+    img_s = procees_image(pair_path + 'pred_target.png', imshow=False, title=None)
+
+    model.train()
+    img = Variable(img_s, requires_grad=True)
+    logo_feat = model(img).squeeze(0)
+
+    t = logo_feat.dot(img_feat)
+
+    t.backward()
+
+    grad = img.grad
+
+    max = grad.view(-1).max()
+    min = grad.view(-1).min()
+
+    grad -= min
+    grad /= (max - min)
+
+
+
+    plt.matshow(grad.squeeze(0).squeeze(0).cpu())
+    plt.title('similarity')
+    plt.show()
+
+# import cv2
+# cv2.imwrite('./test1.png', grad)
+#
+# print(img_o.squeeze(0))
+
+    # max = total.view(-1).max()
+    # min = total.view(-1).min()
+    #
+    # total -= min
+    # total /= (max - min)
+    #
+    # plt.matshow(total.squeeze(0).squeeze(0).cpu())
+    # plt.show()
+
+
+
+
+
+
+
 
 
 
