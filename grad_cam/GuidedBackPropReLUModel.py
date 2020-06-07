3e7b0923e48b07a84c5e8429ea0e68cb94cd30f4
WANG CHAO <1229983126@qq.com>
2020-06-07 18:47:12 +0800

    Change structure

---
+++ grad_cam/GuidedBackPropReLUModel.py
@@ -0,0 +1,43 @@
+import torch
+from torch.autograd import Function
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class GuidedBackPropReLU(Function):
+    @staticmethod
+    def forward(self, input):
+        positive_mask = (input > 0).type_as(input)
+        output = torch.addcmul(torch.zeros(input.size()).type_as(input), input, positive_mask)
+        self.save_for_backward(input, output)
+        return output
+
+    @staticmethod
+    def backward(self, grad_output):
+        input, output = self.saved_tensors
+
+        positive_mask_1 = (input > 0).type_as(grad_output)
+        positive_mask_2 = (grad_output > 0).type_as(grad_output)
+        grad_input = torch.addcmul(torch.zeros(input.size()).type_as(input),
+                                   torch.addcmul(torch.zeros(input.size()).type_as(input), grad_output,
+                                                 positive_mask_1), positive_mask_2)
+
+        return grad_input
+
+
+class GuidedBackPropReLUModel:
+    def __init__(self, model):
+        self.model = model
+        self.model = model.to(device)
+
+        def recursive_relu_apply(module_top):
+            for idx, module in module_top._modules.items():
+                recursive_relu_apply(module)
+                if module.__class__.__name__ == 'ReLU':
+                    module_top._modules[idx] = GuidedBackPropReLU.apply
+
+        # replace ReLU with GuidedBackpropReLU
+        recursive_relu_apply(self.model)
+
+    def forward(self, input):
+        return self.model(input)
