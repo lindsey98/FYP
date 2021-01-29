a227dd03bab8c816d3dfaa125a24ed4a3c3fe104
WANG CHAO <1229983126@qq.com>
2021-01-29 11:55:41 +0800

    16-25

---
+++ ResidualLoss/CNN_l2_30_l_20-21.py
@@ -0,0 +1,120 @@
+import random
+import sys
+
+from torch.autograd import Variable
+from torch import optim
+
+import numpy as np
+from torch.backends import cudnn
+import torch.nn.functional as F
+import torch
+from torch.utils.data import DataLoader, WeightedRandomSampler
+
+from ResidualLoss.dataset import cifar10_data_loader_test, cifar10_data_loader_train, cifar10_dataset_train
+from ResidualLoss.model import CIFAR_17
+
+
+class Logger(object):
+    def __init__(self):
+        self.terminal = sys.stdout
+        log_loc = "./log/%s.txt" % sys.argv[0].split("/")[-1].split(".")[0]
+        self.log = open(log_loc, "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+        self.log.flush()
+
+    def flush(self):
+        pass
+
+
+sys.stdout = Logger()
+
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    cudnn.deterministic = True
+
+
+setup_seed(1914 * 2021)
+num_epochs = 1800
+batch_size = 100
+learning_rate = 0.0001
+
+model = CIFAR_17().cuda()
+model.train()
+
+optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
+
+train_data_loader = cifar10_data_loader_train(batch_size)
+train_data_length = 50000
+test_data_loader = cifar10_data_loader_test(batch_size)
+
+
+def residual_train():
+    total_correct_sum = 0
+    total_classification_loss = 0
+
+    for epoch in range(num_epochs):
+        total_correct = 0
+        model.train()
+        total_train_loss = 0
+
+        for data, target in train_data_loader:
+            data, target = data.cuda(), target.cuda()
+            optimizer.zero_grad()
+
+            output, features = model.features(data)
+            loss = F.nll_loss(output, target)
+            pred = output.argmax(dim=1)
+            total_correct += pred.eq(target).sum().item()
+
+            loss.backward()
+            optimizer.step()
+
+            total_train_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+
+        total_train_loss /= train_data_length
+        total_correct_sum += total_correct
+        total_classification_loss += total_train_loss
+        print('epoch [{}/{}], loss:{:.4f} Accuracy: {}/{}'.format(epoch + 1, num_epochs, total_train_loss, total_correct, train_data_length))
+
+    print("average correct:", total_correct_sum / num_epochs)
+    print("average loss:", total_classification_loss / num_epochs)
+
+
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_data_loader:
+            data, target = data.cuda(), target.cuda()
+            output = model(data)
+
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1)  # get the index of the max log-probability
+            correct += pred.eq(target).sum().item()
+
+    test_loss /= len(test_data_loader.dataset)
+
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_data_loader.dataset),
+        100. * correct / len(test_data_loader.dataset)))
+
+# 1000, 500, 200, 100, 75, 50, 25, 10, 5, 1, 0.5,
+
+
+if __name__ == '__main__':
+    for iter in range(20, 22):
+        print(iter)
+        model.load_state_dict(CIFAR_17().cuda().state_dict())
+        residual_train()
+        loc = "./CNN-l2-30/iter-%s.pt" % iter
+        torch.save(model.state_dict(), loc)
+        print(iter)
