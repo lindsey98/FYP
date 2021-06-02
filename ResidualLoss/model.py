63abe06a6c8064e488d6cb00cf044522f4298ed4
lindsey98 <liny@xgpd7.comp.nus.edu.sg>
2021-06-02 08:20:20 +0800

    Minor

--- ResidualLoss/model.py
+++ ResidualLoss/model.py
@@ -203,10 +203,58 @@ class CIFAR_17(nn.Module):
         x = x.view(x.shape[0], -1)
         return x
     
+class CIFAR_17_Dropout(nn.Module):
+    def __init__(self, head_size=10):
+        super(CIFAR_17_Dropout, self).__init__()
+        
+        self.body = nn.Sequential(OrderedDict([
+            ('cnn1', nn.Sequential(OrderedDict([
+                            ('conv', nn.Conv2d(3, 8, 3, 1, 1)),
+                            ('relu', nn.ReLU(inplace=True)),
+                            ('dropout', nn.Dropout(p=0.2)),
+                            ('pool', nn.MaxPool2d(2))
+                        ]))),
+            ('cnn2', nn.Sequential(OrderedDict([
+                            ('conv', nn.Conv2d(8, 8, 3, 1, 1)),
+                            ('dropout', nn.Dropout(p=0.2)),
+                            ('relu', nn.ReLU(inplace=True)),
+                            ('pool', nn.MaxPool2d(2))
+                        ]))),             
+            ('cnn3', nn.Sequential(OrderedDict([
+                            ('conv', nn.Conv2d(8, 8, 3, 1, 1)),
+                            ('relu', nn.ReLU(inplace=True)),
+                            ('dropout', nn.Dropout(p=0.2)),
+                            ('pool', nn.MaxPool2d(2)),
+                        ])))
+        ]))
+        
+        self.head = nn.Sequential(OrderedDict([
+            ('dense', nn.Sequential(OrderedDict([
+                            ('fc1', nn.Conv2d(8 * 4 * 4, 32, kernel_size=1, bias=True)),  # implement dense layer in CNN way
+                            ('relu', nn.ReLU(inplace=True)),
+                            ('dropout', nn.Dropout(p=0.2)),
+                            ('fc2', nn.Conv2d(32, head_size, kernel_size=1, bias=True)),
+                        ])))
+            ]))
+        
+
+    def features(self, x):
+        feat = self.body(x)
+        feat = x.view(x.shape[0], -1)
+        return feat
+    
+    def forward(self, x):
+        x = self.body(x)
+        x = x.view(x.shape[0], -1, 1, 1) # flatten
+        x = self.head(x)
+        x = x.view(x.shape[0], -1)
+        return x
+
 
     
 KNOWN_MODELS = OrderedDict([
     ('CIFAR17', lambda *a, **kw: CIFAR_17(10, *a, **kw)),
+    ('CIFAR17_dropout', lambda *a, **kw: CIFAR_17_Dropout(10, *a, **kw)),
     ('CIFAR17_add1', lambda *a, **kw: CIFAR_17_Add1(10, *a, **kw)),
     ('CIFAR17_add2', lambda *a, **kw: CIFAR_17_Add2(10, *a, **kw)),
     ('CIFAR17_add3', lambda *a, **kw: CIFAR_17_Add3(10, *a, **kw)),
